0b48f524fc7c8511b7e691fd16202e1354dc0c9f
Sneha Rahate <119409456+SnehaRahate1404@users.noreply.github.com>
2024-03-28 05:14:02 +0000

    data and time

---
+++ 01_basics/date_time.js
@@ -0,0 +1,8 @@
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+// console.log(typeof myDate);
+let newDate = new Date(2028,3,23);
+console.log(newDate.toString())
